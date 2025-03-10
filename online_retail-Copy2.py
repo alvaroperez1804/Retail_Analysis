b3c4d035d3d1fe9bac0f1b80bff299c3b139a561
alvaroperez1804 <alvaroperezb1@gmail.com>
2025-03-10 08:07:36 -0500

    https://github.com/alvaroperez1804/Retail_Analysis.git

--- online_retail-Copy2.py
+++ online_retail-Copy2.py
@@ -64,6 +64,7 @@
 # In[1]:
 
 
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -106,9 +107,9 @@ print(df.count())                #eliminando los valores nulos
 
 print(df.isnull().sum())   #confirmando que no hay valores nulos
 
-df.describe("unitprice","quantity")
+#df.describe("unitprice","quantity") Esto es un error, para seleccionar columnas se usa el []
 
-df[["Quantity","UnitPrice"]].describe()
+print("ver valores atipicos en la tabla:",df[["Quantity","UnitPrice"]].describe())   #nos damos cuenta que hay valores atipicos
 
 indice = df[df["Quantity"] == -80995.000000	 ]     #me da los valores de ese datafram donde quantity es -80995, voy a eliminar esa fila
 print(indice)
@@ -117,4 +118,116 @@ print(indice)
 df = df.drop(df[df["Quantity"] == 80995.000000 ].index)  #siguen habiendo outliers, mejor usamos el metodo IQR para eliminarlos mas adelante
 df.describe()
 
-sns.boxplot(x=df["Quantity"])                 #graficamos el cuantity usand seaborn nos damos cuenta que hay valores atipicos, esto no es bueno cuando vayamos a empezar a hacer calculos de 
+#sns.boxplot(x=df["Quantity"])  #               #graficamos el quantity usand seaborn nos damos cuenta que hay valores atipicos, esto no es bueno cuando vayamos a empezar a hacer calculos de 
+#plt.show()#                                        #Me doy cuenta que tiene 5 outliers
+
+
+Q1 = df["Quantity"].quantile(0.25)
+Q3 = df["Quantity"].quantile(0.75)
+IQR = Q3 - Q1
+limite_inferior = Q1 - 1.5*(IQR)
+limite_superior = Q3 + 1.5*(IQR) 
+df = df[(df["Quantity"] >= limite_inferior)  & (df["Quantity"] <= limite_superior)]
+print("nuevo quantity")
+print(df["Quantity"].describe())    #nOTO QUE AUn hay valores en quantity que son negativos, tengo que eliminarlos
+
+df = df[(df["Quantity"] >= 0)]
+print("Quantity limpiado 100%")
+print(df["Quantity"].describe())
+
+sns.boxplot(df["UnitPrice"])
+plt.title("Buscando outliers en el UnitPrice")
+plt.show()       #veo muchos outliers debemos eliminarlos   ojo las graficas retrasan la lectura
+
+Q1 = df["UnitPrice"].quantile(0.25)
+Q3 = df["UnitPrice"].quantile(0.75)                                                        
+IQR = Q3 - Q1
+limite_inferior = Q1 - 1.5*(IQR)
+limite_superior = Q3 + 1.5*(IQR) 
+df = df[(df["UnitPrice"] >= limite_inferior)  & (df["UnitPrice"] <= limite_superior)]
+print("nuevos valores sin outliers de UnitPrice")
+print(df["UnitPrice"].describe())
+descripcion = df.describe()
+print(descripcion)
+
+sns.boxplot(df["UnitPrice"])
+plt.title("UnitPrice sin outliers")      
+plt.show()    #notese que los outliers que aparecen que estuvieran ahi no son significativos, y son outliers de la nueva muestra
+print(df["CustomerID"].nunique())   #para saber cuantos clientes tiene la tienda osea cuantos valores unicos hay , ok tiene 4192
+print(df["StockCode"].nunique())   #vamos a ver cuantos valores unicos tiene el stockcodem para saber si en verdad el codigo es unico o no, y filtramos el producto que mas se vendio por DESCRIPTION
+print(df["StockCode"].count())     #aunque en realidad esto se podia comparar facilemente 
+mas_vendidos = df["StockCode"].value_counts().head(15)  #vamos a ver cuantas veces se repite cada producto para ver cuales son los mas vendidos
+print(mas_vendidos.head(15))
+print("DIAGRAMA DE BARRAS PARA LOS PRODUCTOS MAS VENDIDOS")
+df["ShortDescription"] = df["Description"].apply(lambda x: ' '.join(str(x).split()[:3]))
+mas_vendidos = df["StockCode"].value_counts().head(15).reset_index()
+mas_vendidos.columns = ["StockCode", "CantidadVendida"]
+
+# Unimos con la descripción corta
+mas_vendidos = mas_vendidos.merge(df[["StockCode", "ShortDescription"]].drop_duplicates(), on="StockCode", how="left")
+
+# Graficamos usando la descripción corta en el eje X
+plt.figure(figsize=(12,8))
+plt.bar(mas_vendidos["ShortDescription"], mas_vendidos["CantidadVendida"], color="blue")
+plt.xlabel("Producto (Descripción Corta)")
+plt.ylabel("Cantidad Vendida")
+plt.title("Top 15 Productos Más Vendidos")
+plt.xticks(rotation=45, ha="right", fontsize=10)
+plt.show()
+
+print(df["Country"].value_counts().head(15))
+paises = df["Country"].value_counts().head(15)
+plt.figure(figsize=(12,8))
+paises.plot(kind="bar",color = "blue")
+plt.title("paises mas vendidos")
+plt.ylabel("Cantidad")
+plt.xlabel("Stockcode", fontsize=14)
+plt.xticks(rotation=45, ha='right', fontsize=12)
+plt.show()    #ya tenemos la grafica de los paises con mas ventas, deifinitivamente UK es el que msa clientes tiene
+#print(df["InvoiceDate"].sort_values(ascending=False).head(10))
+#print(df["InvoiceDate"].sort_values(ascending=False).tail(10))   #NOS DAMOS CUENTA QUE LAS FECHAS VAN DE DEC DE 2010 HASTA DEC DE 2011
+#como podemos representar en que meses se vendio mas? supongo que un histograma
+print(df["InvoiceDate"].dtype)
+df["month"] = df["InvoiceDate"].dt.month  #se crea para sacar solo el mes de esa clase
+#print(df["month"].head(5))
+print("veces que cada mes se repite")
+meses = df["month"].value_counts()
+print(meses)
+# para darle nombre a las columnas se usa el .columns 
+#queria darle nombre a las columnas para que se graficaran porque me piden mostrar que es lo que voy a graficar en cada eje, y para darle nombre a las columnas primero tienen que existir columnas y para que existan debe haber un datafrme
+#vamos a graficar los meses usando el metodo predeterminado
+# Convierte la Serie en DataFrame
+import calendar
+meses = meses.reset_index()   #convertimos la serie en un dataframe 
+meses.columns = ["Meses","numero_de_ventas"]
+meses["Meses"] = meses["Meses"].apply(lambda x: calendar.month_name[x])    #se usa para que los meses en vez de ser numeros sean nombre
+plt.figure(figsize=(10,6))
+plt.bar(meses["Meses"],meses["numero_de_ventas"])
+plt.xlabel("Meses")
+plt.ylabel("# VENTAS")
+plt.xticks(rotation = 45)
+plt.show()
+
+#los clientes que mas compraron
+clientes_masmas = df["CustomerID"].value_counts().reset_index().head(15)
+clientes_masmas.columns = ["Cliente","numero_de_compras"]
+clientes_masmas["Cliente"] = clientes_masmas["Cliente"].astype(str)
+plt.figure(figsize=(10,6))
+plt.bar(clientes_masmas["Cliente"],clientes_masmas["numero_de_compras"])
+plt.xlabel("Cliente")
+plt.ylabel("# compras")
+plt.title("Numero de compras por clienteID")
+plt.xticks(rotation = 45)
+plt.show()
+
+#vamos a hacer las correlaciones
+df["month"] = df["InvoiceDate"].dt.month
+df_analisis_corre = df[["Quantity","UnitPrice","CustomerID","month"]]   # Usa df[["columna"]] si necesitas que el resultado siga siendo un DataFrame.
+print(df_analisis_corre.corr())   
+
+
+plt.scatter(df['UnitPrice'], df['Quantity'])
+plt.xlabel('UnitPrice')
+plt.ylabel('Quantity')
+plt.title('Scatter plot de UnitPrice vs Quantity')
+plt.show()
